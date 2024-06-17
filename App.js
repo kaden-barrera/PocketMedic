13dc4d25bb7168d6608558f45c2dce40cbc56a9f
kaden <kbarrera404@gmail.com>
2024-06-16 21:48:17 -0500

    Added home & assessment screens.

---
+++ App.js
@@ -0,0 +1,21 @@
+// App.js
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import AssessmentScreen from './screens/AssessmentScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Assessment" component={AssessmentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/AssessmentScreen.js
@@ -0,0 +1,11 @@
+// screens/AssessmentScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AssessmentScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Assessment Screen</Text>
+  </View>
+);
+
+export default AssessmentScreen;
